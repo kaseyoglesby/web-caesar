60f97fd0f6d9c90d72cfce0154b603aa3ac3140a
Kasey Oglesby <kasey.oglesby@icloud.com>
2017-03-25 12:08:57 -0500

    Completed rotation, sticky message, and escape

--- main.py
+++ main.py
@@ -14,18 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
-import caesar
+import webapp2, caesar, cgi
+
+def build_page(textarea_content):
+        header = '<h2>Caesar Encryption</h2>'
+        rot_label = "<label>Rotate by: </label>"
+        message_label = "<label>Type a message: </label>"
+        rotation_input = '<input type="number" name="rotation" value="1"/>'
+        textarea = '<textarea name="message">' + textarea_content + '</textarea>'
+        submit = '<input type="submit" />'
+        form = ('<form method="post">' +
+                rot_label + rotation_input + '<br>' +
+                message_label + textarea + '<br>' +
+                submit + '</form>')
+
+        return (header + form)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "test"
-        encrypted_message = caesar.encrypt(message, 2)
 
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type='submit' />"
-        form = "<form>" + textarea + "<br>" + submit + "</form>"
-        self.response.write(form)
+        content = build_page("")
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        encrypted_message = caesar.encrypt(message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
